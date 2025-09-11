75f2bcdb1c6c9308acfe24a4c3ef2c2adc86ad3e
mohammed-akkad <momoo362514@gmail.com>
2025-09-11 17:12:33 +0300

    update actor media layout

--- app/src/main/java/com/karrar/movieapp/ui/actorDetails/actorSocial/SocialItemUIState.kt
+++ app/src/main/java/com/karrar/movieapp/ui/actorDetails/actorSocial/SocialItemUIState.kt
@@ -1,11 +1,7 @@
 package com.karrar.movieapp.ui.actorDetails.actorSocial
 
-import android.annotation.SuppressLint
-import androidx.annotation.StringRes
-import com.karrar.movieapp.R
-
 data class SocialItemUIState(
-    @SuppressLint("ResourceType") @StringRes val iconRes: Int = R.drawable.colored_facebook,
-    val label: String = "",
-    val url: String = ""
+    val iconRes: Int,
+    val label: Int,
+    val url: String
 )

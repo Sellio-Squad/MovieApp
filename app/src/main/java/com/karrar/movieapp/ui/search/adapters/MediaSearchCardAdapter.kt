64549d2f6f5d3073136251ea79fa1c3ac702d516
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-09-13 15:27:55 +0300

    feat: add media search card adapter

---
+++ app/src/main/java/com/karrar/movieapp/ui/search/adapters/MediaSearchCardAdapter.kt
@@ -0,0 +1,25 @@
+package com.karrar.movieapp.ui.search.adapters
+
+import androidx.recyclerview.widget.DiffUtil
+import com.karrar.movieapp.R
+import com.karrar.movieapp.ui.base.*
+import com.karrar.movieapp.ui.search.SearchViewModel
+import com.karrar.movieapp.ui.search.mediaSearchUIState.MediaUIState
+
+
+class MediaSearchCardAdapter(listener: SearchViewModel)
+    : BasePagingAdapter<MediaUIState>(MediaSearchCardComparator, listener){
+    override val layoutID: Int = R.layout.item_media_card_search
+
+    object MediaSearchCardComparator : DiffUtil.ItemCallback<MediaUIState>(){
+        override fun areItemsTheSame(oldItem: MediaUIState, newItem: MediaUIState) =
+            oldItem.mediaID == newItem.mediaID
+
+        override fun areContentsTheSame(oldItem: MediaUIState, newItem: MediaUIState) =
+            oldItem == newItem
+    }
+}
+
+interface MediaSearchCardInteractionListener : BaseInteractionListener {
+    fun onClickMediaResult(media: MediaUIState)
+}

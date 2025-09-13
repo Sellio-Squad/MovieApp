8bf27119cd6a02e85694094c919f9ac4ad33a7ed
marah222 <maraharam222@gmail.com>
2025-09-13 16:57:49 +0300

    feat : crew domain

--- app/src/main/java/com/karrar/movieapp/domain/usecases/tvShowDetails/GetTvShowDetailsUseCase.kt
+++ app/src/main/java/com/karrar/movieapp/domain/usecases/tvShowDetails/GetTvShowDetailsUseCase.kt
@@ -5,6 +5,7 @@ import com.karrar.movieapp.domain.enums.MediaType
 import com.karrar.movieapp.domain.mappers.ListMapper
 import com.karrar.movieapp.domain.mappers.SeriesMapperContainer
 import com.karrar.movieapp.domain.models.Actor
+import com.karrar.movieapp.domain.models.Crew
 import com.karrar.movieapp.domain.models.MediaDetailsReviews
 import com.karrar.movieapp.domain.models.Season
 import com.karrar.movieapp.domain.models.TvShowDetails
@@ -26,7 +27,12 @@ class GetTvShowDetailsUseCase @Inject constructor(
 
     suspend fun getSeriesCast(tvShowId: Int): List<Actor> {
         return ListMapper(seriesMapperContainer.actorMapper)
-            .mapList(seriesRepository.getTvShowCast(tvShowId)?.cast)
+            .mapList(seriesRepository.getTvShowCastAndCrew(tvShowId)?.cast)
+    }
+
+    suspend fun getSeriesCrew(tvShowId: Int): List<Crew> {
+        return ListMapper(seriesMapperContainer.seriesCrewMapper)
+            .mapList(seriesRepository.getTvShowCastAndCrew(tvShowId)?.crew)
     }
 
     suspend fun getSeasons(tvShowId: Int): List<Season> {

1ac4a643721450bcef43fdf7663269a31042019e
Israa Mohamed <e.mohamed@shabek.com>
2025-09-26 22:18:07 +0300

    enable match feature for guest

--- app/src/main/java/com/karrar/movieapp/ui/match/MatchEvent.kt
+++ app/src/main/java/com/karrar/movieapp/ui/match/MatchEvent.kt
@@ -1,11 +1,7 @@
 package com.karrar.movieapp.ui.match
 
-import com.karrar.movieapp.ui.movieDetails.MovieDetailsUIEvent
-
 sealed class MatchEvent {
     data class OnMovieClick(val id: Int) : MatchEvent()
     data class OnSaveClick(val id: Int) : MatchEvent()
     data class OnPlayTrailerClick(val id: Int) : MatchEvent()
-    object ShowLoginDialogEvent : MatchEvent()
-
 }
